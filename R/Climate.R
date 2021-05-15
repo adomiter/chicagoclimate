58e6da45e39891126b3f6fb5e2532609627ca0fc
adomiter <adomiter@uchicago.edu>
2021-05-14 21:31:18 -0500

    Made sure tmap package could be installed, modified description, added license, added Climate data, added the documentation for climate data and choroMap function

---
+++ R/Climate.R
@@ -0,0 +1,14 @@
+#'Climate data for Chicago
+#'
+#'A data set containing data related to climate for Chicago
+#'
+#'
+#'@format A data frame with 14990 rows and 30 variables:
+#'\describe{
+#'  \item{Station}{station, station that collected the data}
+#'  \item{Latitude}{latitude, part of the coordinate}
+#'  ...
+#'}
+#'
+#'@source \url(https://www.ncdc.noaa.gov/cdo-web/)
+"climate"
--- R/choroMap.R
+++ R/choroMap.R
@@ -1,5 +1,20 @@
-#install.packages("tmap")
+
 library(tmap)
+#' Create choropleth map
+#'
+#' Create a choropleth map from a given dataset using
+#' the coloumn of interest
+#'
+#' @param ds dataset
+#' @param c column
+#' @param t title
+#' @param p palette
+#'
+#' @return a map
+#' @export
+#'
+#' @examples
+#' choroMap(US3, "Bush_pct", ""Percentage vote share for Bush - 2004", "Spectral")
 choroMap <- function(ds, c, t, p) {
-  tm_shape(ds) + tm_polygons(col=c, title = t, palette = p) + tm_style_classic() + tm_scale_bar(position = c("right", "top"))
+  tmap::tm_shape(ds) + tmap::tm_polygons(col=c, title = t, palette = p) + tmap::tm_scale_bar(position = c("right", "top"))
 }
